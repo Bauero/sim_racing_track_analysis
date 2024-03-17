2f68d59c719d8b6f746fc31e4f8862b14642236b
Piotr Bauer <piotr.bauer.konto@gmail.com>
2024-03-17 23:25:24 +0100

    Init

---
+++ data_preparation_pandas.py
@@ -0,0 +1,6 @@
+# data = pd.read_csv(path2, skip_blank_lines=True, header=12, index_col="Time",
+#                     delimiter=',',decimal=',')
+
+# data = data.drop("s")
+
+# data = data.astype(float, errors='ignore')
