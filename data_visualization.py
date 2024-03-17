8cd947e4f32fb862773a31c370b11b12ba3c8df7
Piotr Bauer <piotr.bauer.konto@gmail.com>
2024-03-18 00:28:59 +0100

    Prepared option to show speed graph via matplotlib

--- data_visualization.py
+++ data_visualization.py
@@ -6,40 +6,47 @@ necessary to proprerly show properties of raw racing data.
 import os
 from tkinter import filedialog
 import pandas as pd
+# from data_preparation import display_track_summary as __dts, \
+#                              extract_general_data as __egd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-def __read_data_term():
-    ans = input("\033[93mInput file path\033[0m: \033[94m").strip()
-    print("\033[0m", end = "")
-    if os.path.exists(ans):
-        return __read_data(ans)
-    else:
-        return None
+def show_speed_graph_whole(table):
+    laps = table['LAP_BEACON'].unique()
 
+    plt.figure(figsize=(10, 6))
 
-def __read_data_choose_file():
-    ans = filedialog.askopenfilename(filetypes=[("Coma Separated Value","*.csv")])
-    if ans:
-        return ans #open(ans) #__read_data(ans)
-    else:
-        return None
+    # Plot each lap
+    for i, lap in enumerate(laps, start=1):
+        lap_table = table[table['LAP_BEACON'] == lap]
+        plt.plot(lap_table.index, lap_table['SPEED'], label=f'Lap {int(lap)}')
 
-
-def __read_data(path : str):
-    return pd.read_csv(path)
-
-
-def __convert_list_to_DF(data : list[list]) -> pd.DataFrame:
-    return pd.DataFrame(data)
+    plt.title('Speed on All Laps')
+    plt.xlabel('Time')
+    plt.ylabel('Speed (km/h)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 
 if __name__ == "__main__":
-    from data_preparation import prepare_data
-    import csv
-    # values = [a.split(',') for a in ]
-    info, data = prepare_data(list(csv.reader(open(__read_data_choose_file()))),
-                        False,
-                        True,
-                        True)
-    print(__convert_list_to_DF(data).head())
+    path = "/Users/piotrbauer/Documents/SGGW/Semsetr_6/HANZE/" +\
+           "car_data_analysis/cleaned_data_2_3_2024_15_03_03.csv"
+
+    data = pd.read_csv(path, skip_blank_lines=True, header=0, index_col="Time",
+                        delimiter=',',decimal=',')
+
+    exceptions = ['LAP_BEACON']
+    not_exceptions = [x for x in data.columns.to_list() if x not in exceptions]
+
+    data[exceptions] = data[exceptions].astype(int)
+    data[not_exceptions] = data[not_exceptions].astype(float)
+
+    # plt.figure(figsize=(10,6))
+    # wynik = sns.lineplot(data['SPEED'])
+    # plt.show()
+
+    show_speed_graph_whole(data)
+
     exit()
