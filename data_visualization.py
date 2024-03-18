51e8296399c99068a9a9c28f6c6fdb292039d4a4
Piotr Bauer <piotr.bauer.konto@gmail.com>
2024-03-18 16:11:20 +0100

    Comparison of speed over time and distance

--- data_visualization.py
+++ data_visualization.py
@@ -10,7 +10,7 @@ import pandas as pd
 #                              extract_general_data as __egd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 def show_speed_graph_whole(table):
     laps = table['LAP_BEACON'].unique()
@@ -30,23 +30,87 @@ def show_speed_graph_whole(table):
     plt.show()
 
 
+def show_speed_distance_graphs(datasets : list[pd.DataFrame], lap : int = 1):
+    plt.figure(figsize=(10,6))
+    plt.title(f"Car comparison based on Distance - lap {lap}")
+
+    plots = []
+    highiest_distance = 0
+    highiest_speed = 0
+
+    for n, dts in enumerate(datasets):
+        d_ft_d = dts[dts['LAP_BEACON'] == lap]
+        driver_dist, = plt.plot(d_ft_d.Distance, 
+                                d_ft_d['SPEED'],
+                                label = f'Car #{n+1}')
+        plots.append(driver_dist)
+        cms = max(d_ft_d['SPEED'])
+        cmd = max(d_ft_d['Distance'])
+        highiest_speed = cms if cms > highiest_speed else highiest_speed
+        highiest_distance = cmd if cmd > highiest_distance else highiest_distance
+
+    plt.grid(True)
+    plt.legend(handles=plots, loc='upper right')
+    plt.xlabel("Distance [m]")
+    plt.ylabel("Speed [km/h]")
+    plt.yticks(np.arange(0,( highiest_speed // 10 + 1) * 10, 10))
+    plt.xticks(np.arange(0, (highiest_distance // 250 + 1) * 250 + 1, 250))
+
+    return plots
+
+
+def show_speed_time_graphs(datasets : list[pd.DataFrame], lap : int = 1):
+    plt.figure(figsize=(10,6))
+    plt.title(f"Car comparison based on Time - lap {lap}")
+
+    plots = []
+    highiest_time = 0
+    highiest_speed = 0
+
+    for n, dts in enumerate(datasets):
+        d_ft_t = dts[dts['LAP_BEACON'] == lap]
+        driver_dist, = plt.plot(d_ft_t.index, 
+                                d_ft_t['SPEED'],
+                                label = f'Car #{n+1}')
+        plots.append(driver_dist)
+        cms = max(d_ft_t['SPEED'])
+        cmt = max(d_ft_t.index)
+        highiest_speed = cms if cms > highiest_speed else highiest_speed
+        highiest_time = cmt if cmt > highiest_time else highiest_time
+
+    plt.grid(True)
+    plt.legend(handles=plots, loc='upper right')
+    plt.xlabel("Time [s]")
+    plt.ylabel("Speed [km/h]")
+    plt.yticks(np.arange(0, (highiest_speed // 10 + 1) * 10 + 1, 10))
+    plt.xticks(np.arange(0, (highiest_time // 10 + 1) * 10 + 1 , 10))
+
+    return plots
+
+
 if __name__ == "__main__":
     path = "/Users/piotrbauer/Documents/SGGW/Semsetr_6/HANZE/" +\
-           "car_data_analysis/cleaned_data_2_3_2024_15_03_03.csv"
+           "car_data_analysis/cleaned_data_2.3.2024_15-03-03.csv"
+    path2 = "/Users/piotrbauer/Documents/SGGW/Semsetr_6/HANZE/" +\
+           "car_data_analysis/cleaned_data_3.3.2024_17-45-14.csv"
 
     data = pd.read_csv(path, skip_blank_lines=True, header=0, index_col="Time",
+                        delimiter=',',decimal=',')
+    
+    data2 = pd.read_csv(path2, skip_blank_lines=True, header=0, index_col="Time",
                         delimiter=',',decimal=',')
 
     exceptions = ['LAP_BEACON']
     not_exceptions = [x for x in data.columns.to_list() if x not in exceptions]
 
+    data = data.astype(float)
     data[exceptions] = data[exceptions].astype(int)
-    data[not_exceptions] = data[not_exceptions].astype(float)
+    data2 = data2.astype(float)
+    data2[exceptions] = data2[exceptions].astype(int)
 
-    # plt.figure(figsize=(10,6))
-    # wynik = sns.lineplot(data['SPEED'])
-    # plt.show()
+    show_speed_distance_graphs([data, data2])
+    show_speed_time_graphs([data, data2])
 
-    show_speed_graph_whole(data)
+    plt.show()
 
     exit()
