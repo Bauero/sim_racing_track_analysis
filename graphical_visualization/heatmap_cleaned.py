cd61e71af206d58ab25f008d1f5017b536458dd8
Piotr Bauer <piotr.bauer.konto@gmail.com>
2024-06-24 13:59:15 +0200

    Updated structure of heatmap file so it can be executed on analysis

--- graphical_visualization/heatmap_cleaned.py
+++ graphical_visualization/heatmap_cleaned.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import tkinter as TK
-from tkinter.filedialog import askopenfilename
+import os
+import pathlib
+
+
+##########################  CONSTANTS FOR COLORING   ##########################
+
 
 acceptable_time_std = {
     1:  0.5,
@@ -12,7 +16,7 @@ acceptable_time_std = {
     5:  0.3,
     6:  0.2,
     7:  0.3,
-    8:  0.3,
+    8:  0.15,
     9:  0.3,
     10: 0.3,
     11: 0.3,
@@ -20,10 +24,10 @@ acceptable_time_std = {
     13: 0.3,
     14: 0.3,
     15: 0.3,
-    16: 0.3,
-    17: 0.3,
+    16: 0.03,
+    17: 0.15,
     18: 0.3,
-    19: 0.3,
+    19: 0.05,
     20: 0.3,
     21: 0.3,
     22: 0.3,
@@ -31,38 +35,15 @@ acceptable_time_std = {
     24: 0.3,
 }
 
-acceptable_max_speed_std = {
-    1:  5,
-    2:  2,
-    3:  3,
-    4:  1,
-    5:  3,
-    6:  2,
-    7:  3,
-    8:  3,
-    9:  3,
-    10: 3,
-    11: 3,
-    12: 3,
-    13: 3,
-    14: 3,
-    15: 3,
-    16: 3,
-    17: 3,
-    18: 3,
-    19: 3,
-    20: 3,
-    21: 3,
-    22: 3,
-    23: 3,
-    24: 3,
-}
-
 bad_color = [255,0,0]
 avg_color = [255,255,0]
 good_color = [0,255,0]
 
-def return_color(section, value, std_table):
+
+#########################  FUNCITONS USED IN PROGRAM   #########################
+
+
+def __return_color(section, value, std_table):
     
     global bad_color
     global avg_color
@@ -91,8 +72,7 @@ def return_color(section, value, std_table):
     return f"#{end_color[0]:02x}{end_color[1]:02x}{end_color[2]:02x}"
 
 
-
-def return_colors_for_seciton(file : str, 
+def __return_colors_for_seciton(file : str, 
                               value_to_compare : str, 
                               std_table : dict):
     """This funciton allows to genrate colors to all secitons
@@ -109,116 +89,75 @@ def return_colors_for_seciton(file : str,
 
     for row in acceptable_time_std:
         std_time = float(df.iloc[row-1][value_to_compare].replace(",","."))
-        return_dictionary[row] = return_color(row, std_time, std_table)
+        return_dictionary[row] = __return_color(row, std_time, std_table)
 
     return return_dictionary
 
 
+#############################  PUBLIC FUNCTIONS   #############################
 
 
-TK.Tk().withdraw()
-# Load the data from the CSV file
-file_path = askopenfilename(
-        title="Select location of Catalunya_cleaned file",
-        filetypes=[("CSV files", "*.csv")]
-    )
-TK.Tk().destroy()
+def draw_heatmap_for_file(file):
 
-catalunya_data = pd.read_csv(file_path)
+    catalunya_cleaned = pd.read_csv(str(pathlib.Path().resolve()) + 
+                                    "/graphical_visualization/Catalunya_cleaned.csv")
 
-# Extract the coordinates and sections
-x_coords = catalunya_data['Starting_x']
-y_coords = catalunya_data['Starting_y']
-sections = catalunya_data['Section']
+    # Extract the coordinates and sections
+    x_coords = catalunya_cleaned['Starting_x']
+    y_coords = catalunya_cleaned['Starting_y']
+    sections = catalunya_cleaned['Section']
 
-# Determine the rotation angle (90 degrees in radians)
-theta = -0.3185 * np.pi
+    # Determine the rotation angle (90 degrees in radians)
+    theta = -0.3185 * np.pi
 
-# Create the rotation matrix
-rotation_matrix = np.array([
-    [np.cos(theta), -np.sin(theta)],
-    [np.sin(theta), np.cos(theta)]
-])
+    # Create the rotation matrix
+    rotation_matrix = np.array([
+        [np.cos(theta), -np.sin(theta)],
+        [np.sin(theta), np.cos(theta)]
+    ])
 
-# Apply the rotation to each coordinate
-rotated_coords = np.dot(rotation_matrix, np.vstack((x_coords, y_coords)))
+    # Apply the rotation to each coordinate
+    rotated_coords = np.dot(rotation_matrix, np.vstack((x_coords, y_coords)))
 
-# Get unique sections
-unique_sections = sections.unique()
-
-TK.Tk().withdraw()
-file_path2 = askopenfilename(
-        title="Select file with std to analyze",
-        filetypes=[("CSV files", "*.csv")]
-    )
-TK.Tk().destroy()
-
-color_map = return_colors_for_seciton(file_path2, 'Std Time', acceptable_time_std)
+    # Get unique sections
+    unique_sections = sections.unique()
 
 
+    color_map = __return_colors_for_seciton(file, 'Std Time', 
+                                            acceptable_time_std)
 
-# EFFICIENCY !!! - parse file for each seciton ?!?!
+    # Plot each section separately with the specified colors
+    plt.figure(figsize=(16, 8))
 
-def choose_color(file_path3, section_number):
-    # Load the CSV file
-    df = pd.read_csv(file_path3)
-    
-    # Clean up column names
-    df.columns = df.columns.str.replace(' ', '_')
-    
-    # Find the row with the specified section number
-    row = df[df['Section'] == section_number]
-    
-    if not row.empty:
-        # Extract the Std_Avg value
-        std_avg = float(row['Std_Avg'].values[0].replace(',','.'))
-        if std_avg >=10:
-            return 'red'
-        else:
-            return 'blue'
-  
-    else:
-        return None
+    for section in unique_sections:
+        section_coords = rotated_coords[:, sections == section]
+        plt.scatter(section_coords[0], 
+                    section_coords[1], 
+                    label=section, 
+                    c = color_map.get(section),
+                    )
+        
 
-# Define a color map for the sections
-# color_map = {k : choose_color(file_path2, k) for k in range(1,25)}
+    x_min, x_max = np.min(rotated_coords[0]), np.max(rotated_coords[0])
+    y_min, y_max = np.min(rotated_coords[1]), np.max(rotated_coords[1])
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), 
+                         np.linspace(y_min, y_max, 100))
 
+    # Calculate contours
+    levels = np.linspace(np.min(rotated_coords), np.max(rotated_coords), 10)
 
+    # Add contours
+    plt.contour(xx, yy, np.zeros_like(xx), levels = levels, colors = 'black', 
+                alpha=0.3)
 
-
-
-# Plot each section separately with the specified colors
-plt.figure(figsize=(16, 8))
-# plt.axis('off')
-# plt.grid(b=None)
-
-for section in unique_sections:
-    section_coords = rotated_coords[:, sections == section]
-    plt.scatter(section_coords[0], 
-                section_coords[1], 
-                label=section, 
-                c = color_map.get(section),
-                )
-    
-
-x_min, x_max = np.min(rotated_coords[0]), np.max(rotated_coords[0])
-y_min, y_max = np.min(rotated_coords[1]), np.max(rotated_coords[1])
-xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
-
-# Calculate contours
-levels = np.linspace(np.min(rotated_coords), np.max(rotated_coords), 10)
-
-# Add contours
-plt.contour(xx, yy, np.zeros_like(xx), levels=levels, colors='black', alpha=0.3)
-
-plt.title('Circuit de Barcelona-Catalunya Heatmap')
-plt.grid(False)  # Hide grid lines
-plt.axis('equal')
-plt.legend(title='Sections', loc='center left', bbox_to_anchor=(1, 0.5))  # Move legend outside and to the left
-plt.tick_params(axis='both', which='both', bottom=False, top=False, left=False, 
-                right=False, labelbottom=False, labelleft=False)
-x_buffer = (x_max - x_min) * 0.1  # Adjust the buffer as needed
-y_buffer = (y_max - y_min) * 0.1  # Adjust the buffer as needed
-plt.xlim(x_min - x_buffer, x_max + x_buffer)
-plt.ylim(y_min - y_buffer, y_max + y_buffer)
-plt.show()
+    plt.title('Consistency heatmap (base on time)')
+    plt.grid(False)  # Hide grid lines
+    plt.axis('equal')
+    plt.legend(title='Sections', loc='center left', bbox_to_anchor=(1, 0.5))  
+    plt.tick_params(axis='both', which='both', bottom=False, top=False, 
+                    left=False, right=False, labelbottom=False, labelleft=False)
+    x_buffer = (x_max - x_min) * 0.1  # Adjust the buffer as needed
+    y_buffer = (y_max - y_min) * 0.1  # Adjust the buffer as needed
+    plt.xlim(x_min - x_buffer, x_max + x_buffer)
+    plt.ylim(y_min - y_buffer, y_max + y_buffer)
+    plt.show()
